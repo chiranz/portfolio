b59d9435e2c93d5e22a5aeb8442c5b10aa30194f
chiranz <chiranpoudyal@gmail.com>
2019-06-28 12:58:58 +0530

    Loading icon for main page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./pages/Header";
 import Routes from "./routes";
 import SideDrawer from "./components/SideDrawer";
 import BackDrop from "./components/Backdrop";
+import Loading from "./components/Loading";
 
 class App extends Component {
   constructor(props) {
@@ -12,30 +13,37 @@ class App extends Component {
     this.toggleSideDrawer = this.toggleSideDrawer.bind(this);
   }
   state = {
-    sideDrawerOpen: false
+    sideDrawerOpen: false,
+    loading: true
   };
   toggleSideDrawer() {
     this.setState(previousState => ({
       sideDrawerOpen: !previousState.sideDrawerOpen
     }));
   }
-
+  componentDidMount() {
+    setInterval(() => this.setState(() => ({ loading: false })), 2000);
+  }
   render() {
     return (
       <BrowserRouter>
-        <div className="main">
-          {this.state.sideDrawerOpen && (
-            <BackDrop toggleSideDrawer={this.toggleSideDrawer} />
-          )}
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <div className="main">
+            {this.state.sideDrawerOpen && (
+              <BackDrop toggleSideDrawer={this.toggleSideDrawer} />
+            )}
 
-          <SideDrawer
-            toggleSideDrawer={this.toggleSideDrawer}
-            sideDrawerOpen={this.state.sideDrawerOpen}
-          />
+            <SideDrawer
+              toggleSideDrawer={this.toggleSideDrawer}
+              sideDrawerOpen={this.state.sideDrawerOpen}
+            />
 
-          <Header toggleSideDrawer={this.toggleSideDrawer} />
-          <Routes />
-        </div>
+            <Header toggleSideDrawer={this.toggleSideDrawer} />
+            <Routes />
+          </div>
+        )}
       </BrowserRouter>
     );
   }

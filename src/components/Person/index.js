57b46fa312670af6b02a9a547384ce11fa6b7b10
chiranz <chiranpoudyal@gmail.com>
2019-07-02 16:06:42 +0530

    Onclick event for Person component

--- src/components/Person/index.js
+++ src/components/Person/index.js
@@ -1,15 +1,41 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Avatar from "./Avatar";
 import Content from "./Content";
+import { closeSideDrawer } from "../../actions";
 
-export default function index(props) {
+function index(props) {
+  function handleClick() {
+    if (props.isSideDrawerOpen) {
+      props.closeSideDrawer();
+    }
+  }
   return (
     <div className="center person">
-      <Link style={{ textDecoration: "none" }} to="/">
+      <Link
+        style={{ textDecoration: "none" }}
+        to="/"
+        onClick={() => handleClick()}
+      >
         <Avatar small={props.small} />
         <Content small={props.small} />
       </Link>
     </div>
   );
 }
+
+index.propTypes = {
+  isSideDrawerOpen: PropTypes.bool.isRequired,
+  closeSideDrawer: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  isSideDrawerOpen: state.isSideDrawerOpen
+});
+
+export default connect(
+  mapStateToProps,
+  { closeSideDrawer }
+)(index);
